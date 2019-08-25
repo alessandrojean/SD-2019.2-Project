3a009fedb1f9b325b13b2117d24c4dce279ff3ce
Alessandro Jean <alessandrojean@gmail.com>
2019-08-25 14:25:35 -0300

    Corrige um erro de cast que não deveria acontecer.

--- src/main/java/br/edu/ufabc/minitrello/command/CommandNewTask.java
+++ src/main/java/br/edu/ufabc/minitrello/command/CommandNewTask.java
@@ -5,11 +5,17 @@ import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.ZooKeeper;
 import org.apache.zookeeper.ZooDefs.Ids;
+import br.edu.ufabc.minitrello.util.CommandUtils;
 
 public class CommandNewTask implements Command.Call {
 
   @Override
   public void run(Map<String, Object> args) {
+    if (!CommandUtils.argIsString(args.get("task"))) {
+      System.err.println("[ERRO] O argumento 'task' deve ser uma string.");
+      return;
+    }
+
     String task = (String) args.get("task");
     String root = (String) args.get("ROOT");
     ZooKeeper zk = (ZooKeeper) args.get("ZK");
